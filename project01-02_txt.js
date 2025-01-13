e938ba3dce08318295fe22ea7f21f25a01b3502d
Lele92981 <leahliggons@gmail.com>
2025-01-12 22:11:15 -0500

    add project-02

---
+++ project01-02_txt.js
@@ -0,0 +1,26 @@
+
+const service1Name = "Basic";
+const service2Name = "Express";
+const service3Name = "Extreme";
+const service4Name = "Ultimate";
+const service1Speed = "50 Mbps";
+const service2Speed = "100 Mbps";
+const service3Speed = "500 Mbps";
+const service4Speed = "1 Gig";
+
+
+
+document.getElementById("service2").innerHTML = "Express";
+
+
+// document.getElementById("service3").innerHTML = "Extreme";
+
+// document.getElementById("service4").innerHTML = "Ultimate";
+
+// document.getElementById("service1Speed").innerHTML = "0 Mbps";
+
+// document.getElementById("service2Speed").innerHTML = "100 Mbps";
+
+// document.getElementById("service3Speed").innerHTML = "500 Mbps";
+
+// document.getElementById("service4Speed").innerHTML = "1 Gig";
